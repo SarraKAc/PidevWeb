c7eed5a39f7680dba452abf5c4328b3f7c074f10
KarraySlim <Slim.karray21@gmail.Com>
2024-05-02 06:24:12 +0200

    Reduction Metier  +2eme statistique  + pagination

--- src/statistique1.php
+++ src/statistique1.php
@@ -10,7 +10,7 @@ if (!$con) {
 }
 
 // Requête SQL pour récupérer les noms des services et les moyennes d'avis
-$sql = "SELECT service.nom_service, COUNT(avis.nbr_etoile) AS nombreAvis 
+$sql = "SELECT service.nom_service, COUNT(avis.nbr_etoile) AS nombre_avis 
         FROM service 
         INNER JOIN avis ON service.id_service = avis.id_service 
         GROUP BY service.nom_service";
@@ -26,23 +26,31 @@ $moyennes = [];
 while ($data = mysqli_fetch_assoc($req)) {
     $noms[] = $data['nom_service'];
     // Conversion de la moyenne en nombre flottant
-    $moyennes[] = floatval($data['moyenne_avis']);
+    $moyennes[] = floatval($data['nombre_avis']);
 }
 
 // Fermeture de la connexion à la base de données
 mysqli_close($con);
 
 ?>
+
 <script>
-    const ctx = document.getElementById('myChart');
+    const ctx = document.getElementById('myPieChart');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: 'doughnut', // Changer le type de graphique en donut
         data: {
             labels: <?php echo json_encode($noms) ?>,
             datasets: [{
                 label: '# Moyennes des Services',
                 data: <?php echo json_encode($moyennes) ?>,
+                backgroundColor: [
+                    '#0074D9', , '#7FDBFF', '#19A974', '#3D9970',
+                     '#3D9970', '#7FDBFF', '#39CCCC', '#3D9970',
+                    '#0052CC', '#2ECC40', '#7FDBFF', '#01FF70', '#3D9970',
+                    '#004085', '#7FDBFF', '#3D9970',// Couleur de remplissage pour la troisième tranche
+                    // Ajoutez d'autres couleurs de remplissage au besoin
+                ],
                 borderWidth: 1
             }]
         },
