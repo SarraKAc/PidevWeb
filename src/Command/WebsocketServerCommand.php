3642971c29ffdb3548e3f3b539125aaadc8d6ac0
Sarra <sarrakouki22@gmail.com>
2024-05-07 23:30:06 +0200

    Sarra's work

---
+++ src/Command/WebsocketServerCommand.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Command;
+
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use App\Websocket\MessageHandler;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+#[AsCommand(name: 'app:websocket-server')]
+
+class WebsocketServerCommand extends Command
+{
+    protected static $defaultName = "app:websocket-server";
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $port = 3001;
+        $output->writeln("Starting server on port " . $port);
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new MessageHandler()
+                )
+            ),
+            $port
+        );
+        $server->run();
+        return 0;
+    }
+}
